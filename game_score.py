01dc43b1295675794431ddb35fcb953daeec51cc
Sreenaina Lanka <sreenaina0711@gmail.com>
2026-02-06 22:39:16 +0530

    add game_score.py file

--- game_score.py
+++ game_score.py
@@ -1,1 +1,10 @@
+name = input("Enter player name: ")
+games = int(input("Enter games played: "))
+total_score = int(input("Enter total score: "))
 
+average = total_score / games
+
+print(f"Player: {name}")
+print(f"Games Played: {games}")
+print(f"Total Score: {total_score}")
+print(f"Average Score: {average}")
